1baf196e4dffe94483e73c7e3b26aa1a89f73234
Devashish Prasad <devashishkprasad@gmail.com>
2018-07-24 00:13:36 +0530

    Update regression.py

--- Linear-Regression-with-multiple-variable/regression.py
+++ Linear-Regression-with-multiple-variable/regression.py
@@ -2,13 +2,35 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+def runner(x, y, b, alpha, iterations):
+    N = len(x1)
+    for i in range(iterations):
+        loss = (b.T.dot(x)) - y
+        gradient = (x.T.dot(loss)) / N   
+        b = b - alpha * gradient
+        cost = ((b.T.dot(x) - y) ** 2)/(N * 2)
+    return b, cost
+    
+
+
 df = pd.read_excel("data.xls")
 
-x1 = np.asmatrix(df.X2)
-x2 = np.asarray(df.X3)
-y = np.asarray(df.X1)
+x1 = np.array(df.X2)
+x2 = np.array(df.X3)
+N = len(x1)
+x0 = np.ones(N)
+x = np.array([x0,x1,x2])
+y = np.array(df.X1)
 
-# y = t0 + t1x1 + t2x2
-print(x1)
-print(x2)
-print(y)
+# our hypothesis function will be
+# y = bT.x
+
+b = np.matrix([[0],[0],[0]])
+print(b.shape)
+alpha = 0.01
+iterations = 1000
+
+b, cost = runner(x, y, b, alpha, iterations)
+
+print("b = " + str(b) )
+print("cost = " + str(cost))
