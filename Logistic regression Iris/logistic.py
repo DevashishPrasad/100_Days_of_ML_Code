108d27ef1130dae52350b4a5c17cd40bbad849e8
Devashish Prasad <devashishkprasad@gmail.com>
2018-08-04 23:16:14 +0530

    Update logistic.py

--- Logistic regression Iris/logistic.py
+++ Logistic regression Iris/logistic.py
@@ -9,7 +9,6 @@ import numpy as np
 def hypo(B, X):
     ht = X.dot(B)
     gt = 1/(1 + np.exp(-ht))
-    # print(B.shape)
     return gt
 
 def cost(B, X, Y):
@@ -21,32 +20,50 @@ def cost(B, X, Y):
 iris = datasets.load_iris()
 # we only take the first two features.
 X = iris.data[0:100, :2]  
+intercept = np.ones(X[:,1].shape)
+intercept = np.reshape(intercept,(100,1))
 Y = iris.target[0:100]
 # convert data in numpy format
 X = np.asarray(X)
+# add intercept
+X = np.concatenate((intercept, X), axis=1)
 Y = np.asarray(Y)
-B = np.matrix([[0],[0]])
-print(B.shape)
+Y = np.reshape(Y,(100,1))
+B = np.zeros(3)
+B = np.reshape(B, (3,1))
+
 # initializing hyperparameters
-alpha = 0.001
-steps = 1000
+alpha = 0.1
+steps = 100000
 N = len(Y)
 
 # gradient descent runner
 for i in range(steps):
     c = cost(B, X, Y)
     gt = hypo(B, X)
-    gradient = (X.T.dot(gt - Y)) / N        
+    # gradient = (X.T.dot(gt - Y)) / N        
+    gradient = np.dot(X.T, (gt - Y)) / N        
+    # print(gradient.shape)
     B = B - alpha * gradient
-    print((gt - Y).shape)
-    # print(c)
+    print("Step : {} Cost : {}".format(i,c))
 
 print("B = ")
 print(B)
-# plt.title("Iris Flower")
-# plt.scatter(X[:, 0], X[:, 1], c=Y)
-# plt.xlabel('Sepal length')
-# plt.ylabel('Sepal width')
 
+# Actual values to test
+# 5.1 3.5 0
+# 7   3.2 1
+
+# making a prediction
+print("{} {} {}".format(B[0][0],B[1][0],B[2][0]) )
+hypo = B[0][0]*1 + B[1][0]*5.1 + B[2][0]*3.5
+prediction = 1/(1 + np.exp(-hypo))
+
+print(prediction)
+
+plt.title("Iris Flower")
+plt.scatter(X[:, 1], X[:, 2], c=Y)
+plt.xlabel('Sepal length')
+plt.ylabel('Sepal width')
 
 plt.show()
